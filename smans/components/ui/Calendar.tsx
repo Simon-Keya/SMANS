7ebfd3480d84efa34ad97ae0237dde4bf1b24117
Simon Keya <keya8020@gmail.com>
2026-01-26 21:41:51 +0300

    update calendar

--- smans/components/ui/Calendar.tsx
+++ smans/components/ui/Calendar.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import * as React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
+import * as React from "react";
 import { DayPicker } from "react-day-picker";
 
+import { buttonVariants } from "@/components/ui/Button";
 import { cn } from "@/lib/utils";
-import { buttonVariants } from "@/components/ui/button";
+
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
