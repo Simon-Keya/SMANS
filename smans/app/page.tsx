a1f531b68444444c69ad83afb53026d318611632
Simon Keya <keya8020@gmail.com>
2026-02-04 19:53:00 +0300

    commit page

--- smans/app/page.tsx
+++ smans/app/page.tsx
@@ -34,7 +34,7 @@ export default async function DashboardHome() {
       ? Math.round((presentToday / totalAttendanceToday) * 100)
       : 0;
 
-  // Role-based content (greeting + stats)
+  
   const roleData = {
     admin: {
       greeting: "Welcome back, Administrator",
@@ -88,7 +88,7 @@ export default async function DashboardHome() {
         </p>
       </div>
 
-      {/* Stats Grid */}
+    
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
         {data.stats.map((stat, index) => {
           const Icon = stat.icon;
