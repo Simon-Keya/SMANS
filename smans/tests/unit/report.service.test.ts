bbd40d2fd194d59a8efd4c8d03255cdd9ab0f1bd
Simon Keya <keya8020@gmail.com>
2026-04-02 16:06:14 +0300

    commit rm test

--- smans/tests/unit/report.service.test.ts
+++
@@ -1,8 +0,0 @@
-import { ReportService } from "@/lib/services/report.service";
-
-describe("ReportService", () => {
-  it("should generate student report", async () => {
-    const report = await ReportService.generateStudentReport("stu_123");
-    expect(Array.isArray(report)).toBe(true);
-  });
-});
