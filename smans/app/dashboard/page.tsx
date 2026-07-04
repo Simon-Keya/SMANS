2e835bd7542d54100e07016475b72443174307c7
Simon Keya <120686819+Simon-Keya@users.noreply.github.com>
2026-07-04 21:08:54 +0300

    update dashboard

--- smans/app/dashboard/page.tsx
+++ smans/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-// app/dashboard/page.tsx
+// app/dashboard/Homepage.tsx
 import { getCurrentUser } from "@/lib/auth/session";
 import { prisma } from "@/lib/prisma";
 import { redirect } from "next/navigation";
