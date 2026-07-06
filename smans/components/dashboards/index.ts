e81895db41f7f5589f8a4251bc460cc59352bbd1
Simon Keya <120686819+Simon-Keya@users.noreply.github.com>
2026-07-06 18:03:25 +0300

    update Dashboard

---
+++ smans/components/dashboards/index.ts
@@ -0,0 +1,6 @@
+// app/dashboard/components/index.ts
+export { AdminDashboard } from './AdminDashboard';
+export { TeacherDashboard } from './TeacherDashboard';
+export { StudentDashboard } from './StudentDashboard';
+export { ParentDashboard } from './ParentDashboard';
+export { AccountantDashboard } from './AccountantDashboard';
